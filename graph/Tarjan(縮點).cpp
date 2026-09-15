51087f7e89b9d5e77dc46b56a532026e0c5bce30
TSAI YUN WEI <115922370+Dynamix3310@users.noreply.github.com>
2026-09-15 21:52:17 +0800

    Refactor Tarjan's algorithm implementation

--- graph/Tarjan(縮點).cpp
+++ graph/Tarjan(縮點).cpp
@@ -1,171 +1,127 @@
-#include<iostream>
-#include<stdio.h>
-#include<stack>
-#include<queue>
-#include<string.h>
-#include<algorithm> // 為了 max
-
+#include <iostream>
+#include <cstdio>
+#include <stack>
+#include <queue>
+#include <algorithm>
 using namespace std;
+
 #define N 500010
 
-struct nice{
+struct nice {
     int to, next;
-}edges [N << 1], nedges [N <<1];
-int n,m,indexx = 1, indexn = 1;
-int head [N], nhead [N];
+} edges[N << 1], nedges[N << 1];
 
-void wirte(int x, int y)
-{
-    edges [indexx].to = y;
-    edges [indexx].next = head [x];
-    head [x] = indexx++;
-}
-void nwirte (int x, int y) {
-    nedges [indexn].to = y;
-    nedges [indexn].next = nhead [x];
-    nhead [x] = indexn++;
+int n, m, indexx = 1, indexn = 1;
+int head[N], nhead[N];
+
+void wirte(int x, int y) {
+    edges[indexx].to = y;
+    edges[indexx].next = head[x];
+    head[x] = indexx++;
 }
 
-// dfn: 時間戳
-// low: 能追溯到的最早時間戳
-// check: 節點是否已訪問 (用於 main 迴圈)
-// inside: 節點是否在堆疊中
-int dfn [N], low [N], num, sum = 0, check [N], inside [N];
-// ne: 縮點後節點 i 的新編號 (e-BCC 代表元)
-// nw: 縮點後新節點的權重
-// in: 縮點後新節點的入度 (用於拓撲排序)
-int ne [N], nw[N], in[N];
-stack <int> s;
-int F[N]; // 用於拓撲排序 DP
+void nwirte(int x, int y) {
+    nedges[indexn].to = y;
+    nedges[indexn].next = nhead[x];
+    nhead[x] = indexn++;
+}
 
-// 變更點 1: 增加父節點 fa 參數
-void tarjan (int now, int fa) {
-    dfn [now] = low[now] = ++num;
+int dfn[N], low[N], num;
+int check[N], inside[N];
+int ne[N], nw[N], in[N];
+stack<int> s;
+
+void tarjan(int now) {
+    dfn[now] = low[now] = ++num;
     s.push(now);
-    check [now] = inside [now] = 1;
-    for(int i = head [now]; i;i = edges[i].next) {
+    check[now] = inside[now] = 1;
+    for (int i = head[now]; i; i = edges[i].next) {
         int next = edges[i].to;
-        
-        // 變更點 2: 跳過父節點，防止 A->B->A 這樣的立即返回
-        if(next == fa) continue;
-
-        //cout <<now << "?????"<< next << endl;
-        if(!dfn [next])
-        {
-            // 變更點 3: 遞迴時傳入 now 作為父節點
-            tarjan (next, now);
+        if (!dfn[next]) {
+            tarjan(next);
             low[now] = min(low[next], low[now]);
+        } else if (inside[next]) {
+            low[now] = min(dfn[next], low[now]);
         }
-        //
-        else if(inside [next])low [now] = min (dfn [next], low[now]);
-    }
-    //cout << now << endl;
-    if(low[now] == dfn [now]) //
-    {
-        //cout << "!!!!!"<<now <<endl;
-        int q = s.top();
-        while(q != now)
-        {
+    } 
+    
+    if (low[now] == dfn[now]) {
+        while (true) {
+            int q = s.top();
             s.pop();
-            inside [q] = 0;
+            inside[q] = 0;
             ne[q] = now;
-            nw[now]+=nw[q]; //
-            //cout << q << ' ' << ne[q]<<endl;
-            q=s.top();
+            if (q == now) break;
+            nw[now] += nw[q]; // 把點權累加給代表點 now
         }
-        ne[q] = now; //!!!
-        inside[q] = 0;
-        s.pop();
-        // (注意: 原版 5.5 在此處漏加了 nw[now] 自身的權重，
-        //  但 5.8 中修正了。為符合 5.5 模板，這裡保留原樣)
     }
-    return;
 }
 
-// (此函式來自 5.5，用於在縮點後的 DAG/樹 上跑 DP)
-void topo ()
-{
+int F[N];
+
+void topo() {
     queue<int> q;
-    for(int i=1;i<=n;i++)
-    {
-        // 只推送 e-BCC 的代表點 (入度為0)
-        if(!in[i] && ne[i] == i) 
-        {
+    //只把是「代表點」且「入度為 0」的點放進隊列
+    for (int i = 1; i <= n; i++) {
+        if (ne[i] == i && !in[i]) {
             q.push(i);
-            F[i] = nw[i]; //
+            F[i] = nw[i];
         }
-    }
-    while(!q.empty())
-    {
-        int x = q.front();
-        q.pop();
-        for(int i = nhead [x];i;i = nedges[i].next)
-        {
-            int y = nedges[i].to;
-            in[y]--;
-            F[y] = max(F[x]+nw[y],F[y]); //
-            if(!in[y])q.push(y);
-        }
-    }
-}
-
-
-int main()
-{
-    cin >> n >> m;
-    // 讀取點權
-    for(int i=1;i<=n;i++)
-    {
-        int x;
-        cin >> x;
-        nw[i] = x;
-        ne[i] = i; // 初始化 ne 陣列
     }
     
-    for (int i=0;i<m;i++)
-    {
-        int x,y;
-        scanf("%d%d",&x,&y);
-        wirte(x,y);
-        wirte(y,x); // 變更點 4: 添加反向邊，使圖變為無向
-    }
-
-    // 找 e-BCC
-    for (int i = 1;i<=n;i++)
-    {
-        if(!check[i])
-        {
-            // 變更點 5: 傳入 0 (或 -1) 作為根節點的父節點
-            tarjan(i, 0); 
+    while (!q.empty()) {   
+        int x = q.front();
+        q.pop();
+        for (int i = nhead[x]; i; i = nedges[i].next) {
+            int y = nedges[i].to;
+            in[y]--;
+            F[y] = max(F[x] + nw[y], F[y]);
+            if (!in[y]) q.push(y);
         }
     }
+}
 
-    // 重新建圖 (建立 e-BCC 縮點後的樹)
-    for (int now = 1; now<=n; now++)
-    {
-        for(int i = head [now]; i;i = edges[i].next)
-        {
+int main() {
+    // 提升 I/O 速度
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    if (!(cin >> n >> m)) return 0;
+    for (int i = 1; i <= n; i++) {
+        cin >> nw[i];
+    }
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        wirte(x, y);
+    }
+
+    // 找 SCC
+    for (int i = 1; i <= n; i++) {
+        if (!check[i]) tarjan(i);
+    }
+
+    // 重新建圖
+    for (int now = 1; now <= n; now++) {
+        for (int i = head[now]; i; i = edges[i].next) {
             int v = edges[i].to;
-            if(ne[v]!=ne[now])// create new edge (這是一座橋)
-            {
-                nwirte (ne [now],ne [v]); //
-                in [ne [v]]++; //
+            if (ne[v] != ne[now]) {
+                nwirte(ne[now], ne[v]);
+                in[ne[v]]++;
             }
         }
     }
 
-    topo(); //
-    
-    sum = 0;
-    // 找出所有 e-BCC 中 DP 值的最大值
-    for(int i=1;i<=n;i++)
-    {
-        if(ne[i] == i) // 只統計代表點
-        {
-            sum = max(sum, F[i]); //
+    topo();
+
+    //只統計代表點的 F[i]
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        if (ne[i] == i) {
+            sum = max(sum, F[i]);
         }
     }
-    cout << sum << endl; //
+    cout << sum << "\n";
 
     return 0;
 }
